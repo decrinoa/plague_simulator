30f17e25da59851196ebb6e31196acf1f04e1008
Pucci00 <puccidaniele2@gmail.com>
2020-07-09 20:05:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@
 
 int main() {
   Board griglia(200);
-  Disease tetano{0.5, 0.01, 3, 0.01};
+  Disease tetano{0.5, 0.01, 3, 0.5};
 
   griglia.placePeople(699);
-  griglia.placePeople(1,State::Infected);
+  griglia.placePeople(10,State::Infected);
 
   
 
   int frame = 0; 
-    while (griglia.count(State::Infected) > 0) {
+    while (griglia.count(State::Infected) > 0 || griglia.peopleInQuarantine() > 0) {
       std::cout << "S: " << griglia.count(State::Susceptible) << " - I: " << griglia.count(State::Infected) << 
         " - R: " << griglia.count(State::Recovered) << " - Q: " << griglia.peopleInQuarantine() << "\n\n";
     
