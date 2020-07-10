1fe964fe12fb66205423ff6aaccc063fd39d4dfa
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 11:30:33 +0200

    Riportato il tetano agli antichi splendori

--- main.cpp
+++ main.cpp
@@ -5,32 +5,32 @@
 
 int main() {
   Board griglia(200);
-  Disease tetano{0.5, 0.01, 3, 0.5};
+  Disease tetano{0.8, 0.01, 5, 0.5};
 
   griglia.placePeople(699);
   griglia.placePeople(10,State::Infected);
 
   int frame = 0; 
-    while (griglia.count(State::Infected) > 0 /*|| griglia.peopleInQuarantine() > 0 */ ) { 
-      std::cout << "S: " << griglia.count(State::Susceptible) << " - I: " << griglia.count(State::Infected) << 
-        " - R: " << griglia.count(State::Recovered) << " - Q: " << griglia.peopleInQuarantine() << "\n\n";
-    
-      //griglia.print();
-      griglia.draw();
+  while (griglia.count(State::Infected) > 0 /*|| griglia.peopleInQuarantine() > 0 */ ) { 
+    std::cout << "S: " << griglia.count(State::Susceptible) << " - I: " << griglia.count(State::Infected) << 
+      " - R: " << griglia.count(State::Recovered) << " - Q: " << griglia.peopleInQuarantine() << "\n\n";
+    //griglia.print();
+    griglia.draw();
 
-      std::cout << "Frame " << frame << '\n';
+    std::cout << "Frame " << frame << '\n';
       
-      using namespace std::chrono_literals;
-      std::this_thread::sleep_for(10ms);
-      //std::cout << "Press RETURN to continue\n";
-      //std::cin.ignore();
-      system("clear");
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(10ms);
+    //std::cout << "Press RETURN to continue\n";
+    //std::cin.ignore();
+    system("clear");
 
-      griglia.evolve(tetano);
-      griglia.move();
+    griglia.evolve(tetano);
+    griglia.move();
 
-      ++frame;
-    }
+    ++frame;
+  }
+  
   //griglia.print();
   griglia.draw();
 
