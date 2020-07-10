b806da7d1e6bc5fac79d25157bbdf1265e269d86
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 22:08:04 +0200

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -8,51 +8,6 @@
 #include <chrono>
 #include <thread>
 
-//Funzioni non usate
-/*int Board::countInfectedNeighbours(int i) {
-  assert(board_[i] == State::Susceptible);
-  
-  int count = 0;
-  
-  //check only if i it's not at the end of board (right side)
-  if (i % n_ != 0) {
-    if (board_[i - 1] == State::Infected) ++count;
-    //check only if i it's not at the first row
-    if (i / n_ != 0 && board_[i - n_ - 1] == State::Infected) ++count;
-    //check only if i it's not at the last row
-    if (i / n_ != n_ - 1 && board_[i - n_ + 1] == State::Infected) ++count;
-    }
-  //check only if i it's not at the begin of board (left side)
-  if (i % n_ != n_ - 1) {
-    if (board_[i + 1] == State::Infected) ++count;
-    //check only if i it's not at the first row
-    if (i / n_ != 0 && board_[i - n_ + 1] == State::Infected) ++count;
-    //check only if i it's not at the last row
-    if (i / n_ != n_ - 1 && board_[i + n_ + 1] == State::Infected) ++count; 
-  }
-  //check only if i it's not at the first row
-  if (i / n_ != 0 && board_[i - n_] == State::Infected) ++count;
-  //check only if i it's not at the last row
-  if (i / n_ != n_ - 1 && board_[i + n_] == State::Infected) ++ count;
-  
-  return count;
-}*/
-
-/*int Board::countNearerThanRadius(int position, double radius) {
-  assert(board_[position] == State::Susceptible);
-
-  int count = 0;
-
-  for (int i = 0, end = board_.size(); i < end; ++i) {
-    if (board_[i] == State::Infected) {
-      //col = (i%n), row = (i/n)
-      double distance = sqrt((i%n_ - position%n_)*(i%n_ - position%n_) + (i/n_ - position/n_)*(i/n_ - position/n_));
-      if (distance <= radius) ++count;
-    }
-  }
-
-  return count;
-}*/
 
 void Board::change(int i) {  
   switch(board_[i]) {
@@ -83,10 +38,8 @@ void Board::placePeople(int numberOfPeople, State const&state, bool returnFromQu
   //Per capire se placePeople è stato chiamato da evolve (quando una persona guarita ritorna dalla quarantena) o se invece
   //è stato chiamato dall'utente, si è resa pubblica (quindi disponibile all'utente) solo la funzione senza bool.
   //Evolve invece chiamerà la funzione privata con il booleano = true.
-  if (!returnFromQuarantine && displayCreated_) {
+  if (!returnFromQuarantine && window_.isOpen())
     window_.close();
-    displayCreated_ = false;
-  }
   
   std::random_device gen;
   
@@ -305,10 +258,9 @@ void Board::draw(int cellSize, int offset, int frame, std::string const& windowT
   unsigned int windowWidth = (n_ + 2*sqrtPeople + 4*offset)*cellSize;
   unsigned int windowHeight = (n_ + 2*offset)*cellSize;
   
-  if (!displayCreated_) {
+  if (!window_.isOpen()) {
     //create display    
     window_.create(sf::VideoMode( windowWidth, windowHeight), windowTitle, sf::Style::Close);
-    displayCreated_ = true;
   }
 
   //update display
@@ -460,7 +412,6 @@ void Board::animate(Disease const& disease, int infectedBeforeQuarantine, bool f
   std::cin.ignore();
 
   window_.close();
-  displayCreated_ = false;  
 }
 
 void Board::save(std::string const& fileName) {
