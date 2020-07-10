b030439263e84def8a7a78e8ebd0bc820bb4edf7
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 23:34:08 +0200

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -8,6 +8,51 @@
 #include <chrono>
 #include <thread>
 
+//Funzioni non usate
+/*int Board::countInfectedNeighbours(int i) {
+  assert(board_[i] == State::Susceptible);
+  
+  int count = 0;
+  
+  //check only if i it's not at the end of board (right side)
+  if (i % n_ != 0) {
+    if (board_[i - 1] == State::Infected) ++count;
+    //check only if i it's not at the first row
+    if (i / n_ != 0 && board_[i - n_ - 1] == State::Infected) ++count;
+    //check only if i it's not at the last row
+    if (i / n_ != n_ - 1 && board_[i - n_ + 1] == State::Infected) ++count;
+    }
+  //check only if i it's not at the begin of board (left side)
+  if (i % n_ != n_ - 1) {
+    if (board_[i + 1] == State::Infected) ++count;
+    //check only if i it's not at the first row
+    if (i / n_ != 0 && board_[i - n_ + 1] == State::Infected) ++count;
+    //check only if i it's not at the last row
+    if (i / n_ != n_ - 1 && board_[i + n_ + 1] == State::Infected) ++count; 
+  }
+  //check only if i it's not at the first row
+  if (i / n_ != 0 && board_[i - n_] == State::Infected) ++count;
+  //check only if i it's not at the last row
+  if (i / n_ != n_ - 1 && board_[i + n_] == State::Infected) ++ count;
+  
+  return count;
+}*/
+
+/*int Board::countNearerThanRadius(int position, double radius) {
+  assert(board_[position] == State::Susceptible);
+
+  int count = 0;
+
+  for (int i = 0, end = board_.size(); i < end; ++i) {
+    if (board_[i] == State::Infected) {
+      //col = (i%n), row = (i/n)
+      double distance = sqrt((i%n_ - position%n_)*(i%n_ - position%n_) + (i/n_ - position/n_)*(i/n_ - position/n_));
+      if (distance <= radius) ++count;
+    }
+  }
+
+  return count;
+}*/
 
 void Board::change(int i) {  
   switch(board_[i]) {
@@ -261,6 +306,8 @@ void Board::draw(int cellSize, int offset, int frame, std::string const& windowT
   if (!window_.isOpen()) {
     //create display    
     window_.create(sf::VideoMode( windowWidth, windowHeight), windowTitle, sf::Style::Close);
+    window_.setVerticalSyncEnabled(true);
+    window_.setPosition(sf::Vector2i{100,0});
   }
 
   //update display
@@ -408,10 +455,16 @@ void Board::animate(Disease const& disease, int infectedBeforeQuarantine, bool f
   draw(cellSize, offset, frame, windowTitle);
 
   std::cout << "Simulation ended.\n";
-  std::cout << "Press RETURN to close the window\n";
-  std::cin.ignore();
-
-  window_.close();
+  std::cout << "Press a key or close the window to continue\n";
+  
+  sf::Event event;
+  //La finestra si può chiudere sia cliccando la x che premendo un tasto
+  while (window_.isOpen()) {
+    if (window_.pollEvent(event)) {
+      if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed)
+        window_.close();
+    }
+  }
 }
 
 void Board::save(std::string const& fileName) {
