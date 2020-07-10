98afdbc26f18e81809292693556683553ce540ae
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 14:13:58 +0200

    Aggiunto l'esempio commentato

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include <thread>
 #include "board.h"
 
-bool frameByFrame = true;
-
 int main() {
   Board griglia(200);
   Disease tetano{0.8, 0.01, 5, 0.5};
@@ -12,6 +10,38 @@ int main() {
   griglia.placePeople(699);
   griglia.placePeople(10,State::Infected);
 
+  //ESEMPIO DI UTILIZZO DELLA CLASSE SENZA LA FUNZIONE Board::animate()
+  /*
+  bool frameByFrame = false;
+
+  int frame = 0; 
+  while (griglia.count(State::Infected) > 0) {
+    std::cout << "S: " << griglia.count(State::Susceptible) << " - I: " << griglia.count(State::Infected) << 
+      " - R: " << griglia.count(State::Recovered) << " - Q: " << griglia.peopleInQuarantine() << "\n\n";
+    griglia.draw();
+    std::cout << "Frame " << frame << '\n';
+    
+    if (frameByFrame) {
+      std::cout << "Press RETURN to continue\n";
+      std::cin.ignore();
+    }
+    else {
+      using namespace std::chrono_literals;
+      std::this_thread::sleep_for(10ms);
+    }
+    system("clear");
+
+    griglia.evolve(tetano);
+    griglia.move();
+    ++frame;
+  }
+
+  griglia.draw();
+  std::cout << "END OF SIMULATION: Press return to close the window \n";
+  std::cin.ignore();    */
+  
+  //FINE ESEMPIO. E' del tutto equivalente a chiamare la funzione animate
+  
   griglia.animate(tetano);
   
   griglia.save("data.dat");
