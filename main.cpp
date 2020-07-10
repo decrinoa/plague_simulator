015fcf7ee612801af88e54cebf606b57521eeed4
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 12:27:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <thread>
 #include "board.h"
 
+bool frameByFrame = true;
+
 int main() {
   Board griglia(200);
   Disease tetano{0.8, 0.01, 5, 0.5};
@@ -10,33 +12,8 @@ int main() {
   griglia.placePeople(699);
   griglia.placePeople(10,State::Infected);
 
-  int frame = 0; 
-  while (griglia.count(State::Infected) > 0 /*|| griglia.peopleInQuarantine() > 0 */ ) { 
-    std::cout << "S: " << griglia.count(State::Susceptible) << " - I: " << griglia.count(State::Infected) << 
-      " - R: " << griglia.count(State::Recovered) << " - Q: " << griglia.peopleInQuarantine() << "\n\n";
-    //griglia.print();
-    griglia.draw();
-
-    std::cout << "Frame " << frame << '\n';
-      
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for(10ms);
-    //std::cout << "Press RETURN to continue\n";
-    //std::cin.ignore();
-    system("clear");
-
-    griglia.evolve(tetano);
-    griglia.move();
-
-    ++frame;
-  }
-  
-  //griglia.print();
-  griglia.draw();
-
-  std::cout << "Press RETURN to close this window\n";
-  std::cin.ignore();
+  griglia.animate(tetano, 1, true);
   
   griglia.save("data.dat");
-  std::cout << "Data saved.\n";
+  
 }
