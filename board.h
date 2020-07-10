cb7d75d174588bc6af408d54e0091bb8da9df8b6
Pucci00 <puccidaniele2@gmail.com>
2020-07-10 13:02:31 +0200

    Aggiunto qualche const&

--- board.h
+++ board.h
@@ -33,7 +33,7 @@ class Board {
   //int countInfectedNeighbours(int i);
   //int countNearerThanRadius(int position, double radius);
 
-  //cambia lo state della cella i: E->S S->I, I->R
+  //cambia lo state della cella i: E->S, S->I, I->R
   void change(int i);
   inline double distance(int i, int j) {     //get distance between two cells
     return sqrt((i%n_ - j%n_)*(i%n_ - j%n_) + (i/n_ - j/n_)*(i/n_ - j/n_)); }
@@ -45,6 +45,7 @@ public:
   void setInfected(int row, int col);  //place an infected person on the board
   inline int peopleInQuarantine() { return peopleInQuarantine_;};
   
+  //Conta il numero di individui allo stato state
   int count(State const& state);
   inline int countPeople() { return n_*n_ - count(State::Empty); };
   void placePeople(int numberOfPeople, State const&state = State::Susceptible); //places randomly nPeople on the board
@@ -65,7 +66,7 @@ public:
   void animate(Disease const& disease, int infectedBeforeQuarantine = 1, bool move = true, bool frameByFrame = false,
                 int cellSize = -1, int offset = 2, std::string const& windowTitle = "Plague simulator");
 
-  void save(std::string fileName = "output.dat"); //stampa su doc dati per grafici 
+  void save(std::string const& fileName = "output.dat"); //stampa su doc dati per grafici 
 };
 
 #endif  //BOARD_H
